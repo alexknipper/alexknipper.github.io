7b08969729dc1c2b65f3951bec41547c9ea82676
Alex Knipper <aknipper115@gmail.com>
2020-12-20 14:50:21 -0600

    Begin animation code

---
+++ _static/scripts/ak-bg-anim.js
@@ -0,0 +1,26 @@
+// ak-bg-anim.js
+// This file is designed to accompany the ak-bg-anim.css stylesheet and provide several critical functions
+// Alex Knipper
+
+function addAKBgAnimEvents() {
+    // Add the '' event to the <>
+}
+
+function selectAnimFromDate() {
+    // Get the current system date
+    date = 
+}
+
+function startAnimCycle(animName, parameters) {
+    // Select parameters based on the animation name
+    switch (animName) {
+        case 'starfall':
+            break;
+        case 'magic':
+            break;
+        case 'snow':
+            break;
+        default:
+            break;
+    }
+}
