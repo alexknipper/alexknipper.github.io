f110322f1a610ecb08e437dd9ab03281228ce2aa
KSeeker5 <aknipper115@gmail.com>
2021-10-07 23:32:33 -0500

    Add more quotes

--- _static/scripts/ak-quote.js
+++ _static/scripts/ak-quote.js
@@ -4,10 +4,6 @@
 
 var quotes = [
     [
-        "Do the thing and win the stuff.",
-        "Alex Knipper"
-    ],
-    [
         "If we give up before we try, then nothing is going to happen.",
         "Eli Ayase"
     ],
@@ -22,6 +18,110 @@ var quotes = [
     [
         "If you've got passion, you can do anything.",
         "Dia Kurosawa"
+    ],
+    [
+        "Life isn’t just doing things for yourself. It’s possible to live in such a way that other people’s happiness makes you happy too.",
+        "Asuna Yuuki"
+    ],
+    [
+        "It's impossible to work hard for something you don't enjoy.",
+        "Keiko Ayano"
+    ],
+    [
+        "The world is not cut from the same cloth. It’s because it is overflowing with inexplicable, unidentifiable things that the world is so beautiful.",
+        "Satsuki Kiryuin"
+    ],
+    [
+        "We’ve all lost something, and I’ve seen what loss can do to people. But if we gave up every time we lost, then we’d never be able to move forward.",
+        "Ruby Rose"
+    ],
+    [
+        "Emotions can grant you strength, but you must never let them overpower you.",
+        "Winter Schnee"
+    ],
+    [
+        "As you live you lose reasons and hope. But as you keep on going, you pick up new reasons and hope.",
+        "Asta"
+    ],
+    [
+        "Being weak is nothing to be ashamed of… Staying weak is!",
+        "Fuegoleon Vermillion"
+    ],
+    [
+        "When you convince yourself you can’t do anymore, you’re finished. Even though you may still have some strength left that you’re just unaware of.",
+        "Yomikawa Aiho"
+    ],
+    [
+        "If you can just nurture the strength to stick to your convictions, the results you seek are sure to follow in time.",
+        "Shirai Kuroko"
+    ],
+    [
+        "The difference between the novice and the master is that the master has failed more times than the novice has tried.",
+        "Ryushi Korogane"
+    ],
+    [
+        "Broadly speaking, there are two reasons a person feels the desire to teach something: Either he wants to pass on his successes or he wants to pass on his failures.",
+        "Ryushi Korogane"
+    ],
+    [
+        "The past you’ve lost will never come back. I myself have made so many mistakes… But we can learn from the past so we don’t repeat it.",
+        "Ryushi Korogane"
+    ],
+    [
+        "Honestly, there isn’t much meaning to the splendid names given to you by your parents. What does have meaning is what the person behind that name does during their actual lifetime. The name doesn’t make the person. The name simply remains gently within the footprint left on the path a person walks.",
+        "Ryushi Korogane"
+    ],
+    [
+        "Even if we forget the faces of our friends, We will never forget the bonds that were carved into our souls.",
+        "Yuzuru Otonashi"
+    ],
+    [
+        "If there’s something you’re displeased with, fight! Become able to say no clearly to things you dislike, for the sake of protecting yourself.",
+        "Fran"
+    ],
+    [
+        "You don't get the things you dream of, you get the things you work for.",
+        "Ursula Callistis"
+    ],
+    [
+        "Don’t act because something is interesting, act because you want to find something more interesting.",
+        "Junichirou Kagami"
+    ],
+    [
+        "Live by your own rules. And have reality accept your own rules. If you are able to do that, the future is all yours.",
+        "Junichirou Kagami"
+    ],
+    [
+        "No matter how hard you’ve agonized over your path, you’ll always regret something about it later.",
+        "Glenn Radars"
+    ],
+    [
+        "The ones who accomplish true greatness are the fools who keep pressing onward. The ones who accomplish nothing are the wise who know when to quit.",
+        "Celica Arfonia"
+    ],
+    [
+        "Do the thing and win the stuff.",
+        "Alex Knipper"
+    ],
+    [
+        "We simply cannot see the world as it is. The world people see is nothing more than how our brains translate it. In other words, it’s subjective.",
+        "Alis Color"
+    ],
+    [
+        "There are some people who are rich and stay in beautiful places but are still unhappy. There are some people who are poor, but happy. In the end, happiness or sadness, only the person can determine.",
+        "Alicia Florence"
+    ],
+    [
+        "Choosing your calling based solely on your talents won’t always guarantee you happiness.",
+        "Kaminski Natalia"
+    ],
+    [
+        "Even a child that receives one bit of praise has the ability to excel in a single talent, and those who receive regular encouragement can feel confidence, achieve success, and become leading members of society. Because they don’t believe they are worthless, they don’t need to raise a fist and have vengeance against fate or the world at large.",
+        "Lunge"
+    ],
+    [
+        "People who neglect to make efforts or who don’t take any actions at all are always the ones who dream that someday they will suddenly become wildly successful.",
+        "Misaki Nakahara"
     ]
 ]
 
